48eddb5e759857f3ae32acf28314aeb1deef1987
Pratysh Anand <anandpratyush1666@gmail.com>
2024-05-14 12:07:19 +0530

    added static pages for HTML,CSS and JS content

---
+++ src/pages/Learner/CSS/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Sidebar from "../../../components/TextContent/SideBar/Sidebar";
+import Header from "../../../components/TextContent/Header/Header"
+import Body from "../../../components/TextContent/Body/Body"
+import Topbar from "../../../components/Topbar/Topbar";
+const CssPage = () => {
+    /*const topics =["C Home", "C Intro", "C Get Started", "C Syntax", "C Output", "C Comments", "C Variables", "C Data Types", "C Constants", "C Operators", "C Strings", "C Math", "C Booleans", "C If..Else", "C Switch", "C While Loop", "C For Loop", "C Break/Continue", "C Arrays", "C User Input", "C Memory Address", "C Pointers", "C Functions", "C Function Parameters", "C Function Declaration", "C Recursion"]
+    return (
+        
+            <div className="flex">
+                 <div className="fixed w-screen"><Topbar/></div>
+                <div className="flex-initial w-1/8">
+                       
+                         <Sidebar items={topics} language={"C"}/>
+                </div>
+                <div className="flex-col">
+                    <div className="flex-initial w-1/8">
+                         <Header/>
+                        
+                    </div>
+                    <div className="flex-initial w-7/8"> 
+                         <Body/>
+                    </div>
+                </div>
+            </div>
+    )*/
+
+    return (
+    <h1>This is CSS Content Page </h1>
+    );
+}
+
+export default CssPage
